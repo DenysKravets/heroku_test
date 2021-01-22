d8a7ca52f77387f3f1c75d44dc1f336e11879027
Denys <denys.lviv2013@gmail.com>
2021-01-22 18:26:36 +0200

    first commit

---
+++ src/test/java/ua/lviv/heraku_test/HerakuTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.lviv.heraku_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerakuTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
